8f473e7bfd3321fa4cc944a99c791e14ce0f2854
atostudios <anthony.thomas.oneill@gmail.com>
2016-09-17 11:23:10 +0100

    added .reduce and .forEach in place of .map

--- src/reverse_translator.js
+++ src/reverse_translator.js
@@ -7,67 +7,52 @@ function ReverseTranslator(dictionary) {
   }
 
   var errorProneProp;
-  var typeTest = Object.keys(dictionary).some(function(item) {
-      errorProneProp = item;
-      return typeof dictionary[item] !== 'string' && typeof dictionary[item] !== 'object';
+  var typeTest = Object.keys(dictionary).some(function(current) {
+      errorProneProp = current;
+      return typeof dictionary[current] !== 'string' && typeof dictionary[current] !== 'object';
   })
 
   if (typeTest) {
     return new Error('Unsupported data type supplied for ' + errorProneProp + ' property');
   }
 
-  var arrayTest = Object.keys(dictionary).some(function(item) {
-    errorProneProp = item;
-    return dictionary[item].length === undefined;
+  var arrayTest = Object.keys(dictionary).some(function(current) {
+    errorProneProp = current;
+    return dictionary[current].length === undefined;
   });
 
   if (arrayTest) {
     return new Error('object supplied for ' + errorProneProp + ' instead of array');
   }
 
-  var reversedDictionary = {}
-  Object.keys(dictionary).map(function(item) {
-    if (typeof dictionary[item] === 'string') {
-      if (!reversedDictionary[dictionary[item]]) {
-        reversedDictionary[dictionary[item]] = item;
-      } else if (typeof reversedDictionary[dictionary[item]] === 'object') {
-        reversedDictionary[dictionary[item]].push(item);
-      } else if (typeof reversedDictionary[dictionary[item]] === 'string') {
-        reversedDictionary[dictionary[item]] = [reversedDictionary[dictionary[item]], item]
+  var reversedDictionary = Object.keys(dictionary).reduce(function(previous, current) {
+    if (typeof dictionary[current] === 'string') {
+      if (!previous[dictionary[current]]) {
+        previous[dictionary[current]] = current;
+      } else if (typeof previous[dictionary[current]] === 'object') {
+        previous[dictionary[current]].push(current);
+      } else if (typeof previous[dictionary[current]] === 'string') {
+        previous[dictionary[current]] = [previous[dictionary[current]], current]
       }
-    } else if (typeof dictionary[item] === 'object') {
-      dictionary[item].map(function(value, index) {
-        if (!reversedDictionary[dictionary[item][index]]) {
-          reversedDictionary[dictionary[item][index]] = item;
+    } else if (typeof dictionary[current] === 'object') {
+      dictionary[current].forEach(function(value, index) {
+        if (!previous[dictionary[current][index]]) {
+          previous[dictionary[current][index]] = current;
         }
-        else if (typeof reversedDictionary[dictionary[item][index]] === 'object') {
-          reversedDictionary[dictionary[item][index]].push(item);
+        else if (typeof previous[dictionary[current][index]] === 'object') {
+          previous[dictionary[current][index]].push(current);
         }
-        else if (typeof reversedDictionary[dictionary[item][index]] === 'string') {
-          reversedDictionary[dictionary[item][index]] = [reversedDictionary[dictionary[item][index]], item]
+        else if (typeof previous[dictionary[current][index]] === 'string') {
+          previous[dictionary[current][index]] = [previous[dictionary[current][index]], current]
         }
       })
     }
 
-  });
+    return previous;
+
+  }, {});
 
   return reversedDictionary;
 }
 
-
-
 module.exports = ReverseTranslator;
-
-
-
-// Object.keys(words).map(function(word) {
-//   var matchWord = new RegExp('\\b' + word + '\\b', "g");
-//   if ( matchWord.test(message) ) {
-//     if ( typeof words[word] === 'object' ) {
-//       var choice = Math.floor(Math.random() * words[word].length);
-//       message = message.replace(matchWord, words[word][choice]);
-//     } else {
-//       message = message.replace(matchWord, words[word]);
-//     }
-//   }
-// });
