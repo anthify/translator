dce60a03a24fef44736f32e02e493b8d1f0d34c4
atostudios <anthony.thomas.oneill@gmail.com>
2016-09-27 16:47:06 +0100

    added percentage function

---
+++ src/translation_object.js
@@ -0,0 +1,17 @@
+ function Translation(message, words, phrases) {
+  this.message = message;
+  this.translation = message;
+  this.translations = [];
+  this.word_count = message.split(' ').length;
+  this.percentage = function() {
+    let translations = this.translations.reduce((n, t) => {
+      n += t.translation.split(' ').length
+      return n;
+    }, 0);
+    return ((this.word_count - translations) / this.word_count) * 100;
+  };
+  this.words = words;
+  this.phrases = phrases;
+};
+
+module.exports = Translation;
