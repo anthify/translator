1a3b75c114cbc63765c2bfcbeec5a8a250f0b0cc
atostudios <anthony.thomas.oneill@gmail.com>
2016-09-28 12:11:20 +0100

    refactored issue with percentage calculation

--- src/translation_object.js
+++ src/translation_object.js
@@ -8,7 +8,10 @@
       n += t.translation.split(' ').length
       return n;
     }, 0);
-    return ((this.word_count - translations) / this.word_count) * 100;
+    if (translations >= this.word_count) {
+      return 100;
+    }
+    return 100 - (((this.word_count - translations) / this.word_count) * 100);
   };
   this.words = words;
   this.phrases = phrases;
